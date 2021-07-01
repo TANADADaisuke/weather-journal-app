23565c84695aff0d0ebc3b1db32b14fa9ed14025
TANADADaisuke <nvkx873110@gmail.com>
2021-07-01 20:41:43 +0900

    feat: Setup express app instance

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
